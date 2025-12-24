61c27f9bf4cff123428eb2874fd4f9f01b86ac8e
voidsaken-script <sigmasigmaboy283@outlook.com>
2025-12-24 23:22:58 +0000

    Update scriptblox_api.py

--- scriptblox_api.py
+++ scriptblox_api.py
@@ -1,4 +1,5 @@
 import tls_client
+import requests
 from random import randint
 
 chromeIdentifier = "chrome" + str(randint(112, 120))
@@ -19,3 +20,14 @@ class Posts:
 		if res.status_code != 200:
 			return False
 		return res.json()
+	
+def _createRealisticRequest(method, url, headers={}, json={}):
+    headers["host"] = "scriptblox.com"
+    headers["accept-encoding"] = "gzip, deflate, br, zstd"
+    headers["referer"] = url
+    headers["content-length"] = str(randint(39, 99))
+    headers["origin"] = "https://scriptblox.com"
+    headers["connection"] = "keep-alive"    
+    headers["user-agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:145.0) Gecko/20100101 Firefox/145.0"
+    headers["accept"] = "application/json"
+    return requests.request(method, url, headers=headers, json=json)
